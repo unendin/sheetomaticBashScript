4403e88d5b69c85d85254e6a6f324721721fa621
Ted Metcalfe <ted.metcalfe@gmail.com>
2014-09-30 15:37:59 -0400

    more test variables and cleaner handling

--- sheetomatic.sh
+++ sheetomatic.sh
@@ -8,41 +8,58 @@
 ##########################
 # CONFIGURE
 
-# Google Drive app location
-driveApp='/Applications/Google Drive.app'
+# TEST flag
+testing='false'
 
-# Google Drive app sync time, after which process is killed
-driveSyncWindow=10
+# TEST settings
+if [ $testing='true' ]; then
+	echo "TESTING TESTING TESTING"
 
+	# Shorten time before quitting drive app
+	driveSyncWindow=10
+
+	# Destination Google Drive directory
+	destinationDir="/Users/$user/Google Drive/logsTest"
+
+	# Artificially set modified date back
+	modifiedSince="$(date  -v-5d "+%Y%m%d%H%M.%S")"
+	echo "Last execution time set to 5 days ago: $modifiedSince."
+
+# PRODUCTION settings
+else
+	# Google Drive app sync time, after which process is killed
+	driveSyncWindow=120	
+
+	# Destination Google Drive directory
+	destinationDir="/Users/$user/Google Drive/logs"
+
+	# Restrict to log files modified since last execution
+	if [ -f /tmp/sheetomatic.txt ]; then
+		read modifiedSince < /tmp/sheetomatic.txt
+		echo "Previous execution time: $modifiedSince."
+
+	# In the absence of stored execution date, restrict search to files 
+	# modified within last 24hr
+	else
+		modifiedSince="$(date  -v-1d "+%Y%m%d%H%M.%S")"
+		echo "No record of previous execution time. Copying logs modified since: $modifiedSince"
+	fi
+fi
+
+# UNIVERSAL settings
 # Current user environment for purposes of defining directories
 user="$(echo $USER)"
 
+# Google Drive app location
+driveApp='/Applications/Google Drive.app'
+
 # Directory containing original logs
 sourceDir="/Users/$user/Dropbox/CodeNData/Data/Shadlen/+MDD/+expr"
-
-# Destination Google Drive directory
-destinationDir="/Users/$user/Google Drive/logs"
 
 # Regexes to match diary filenames only and exclude json variants.
 # Note: 'find' command does not support lookbehind to do both regexes in one
 regex='.*diary_.*.txt'
 notRegex='.*diary_.*json.txt' 
-
-# Restrict to log files modified since last execution
-if [ -f /tmp/sheetomatic.txt ]; then
-    read modifiedSince < /tmp/sheetomatic.txt
-
-    # testing
-    # modifiedSince="$(date  -v-5d "+%Y%m%d%H%M.%S")"
-
-    echo "Last execution time: $modifiedSince."
-
-# In the absence of stored execution date, restrict search to files 
-# modified within last 24hr
-else
-    modifiedSince="$(date  -v-1d "+%Y%m%d%H%M.%S")"
-    echo "No record of previous execution time. Copying logs modified since: $modifiedSince"
-fi
 
 ##########################
 # COPY TO DRIVE FOLDER 
@@ -51,8 +68,10 @@ fi
 touch -t "$modifiedSince" /tmp/logTimestamp
 
 # Find and copy relevant log files
-find "$sourceDir" -not -regex "$notRegex" -regex "$regex"
-find "$sourceDir" -not -regex "$notRegex" -regex "$regex" -exec cp {} "$destinationDir" \;
+find "$sourceDir" -newer /tmp/logTimestamp -not -regex "$notRegex" -regex "$regex" -exec cp {} "$destinationDir" \;  
+
+# for terminal display TODO: non-redundant
+find "$sourceDir" -newer /tmp/logTimestamp -not -regex "$notRegex" -regex "$regex" 
 
 ##########################
 # REPORT 
@@ -69,7 +88,7 @@ echo $modifiedSince > /tmp/sheetomatic.txt
 # SYNC W/GOOGLE
 
 # If files are copied, start Google Drive app, wait, then quit
-if [[ -n $(find "$destinationDir" -newer /tmp/logTimestamp -regex "$regex") ]]; then
+if [[ -n $(find "$sourceDir" -newer /tmp/logTimestamp -not -regex "$notRegex" -regex "$regex") ]]; then
 	echo "Launching Google Drive App ..."
 	open -a "$driveApp"
 	echo "Waiting $driveSyncWindow seconds for Google Drive to sync ..."	
